d2e13b4ddb9f453590d6c7ccca4bf8f0e06916f6
DmitriyColeman <47474886+DmitriyColeman@users.noreply.github.com>
2023-05-13 12:26:49 +0300

    32

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,23 @@ const cors = require( "cors" )
 const cookie_parser = require( "cookie-parser" )
 const mongoose = require('mongoose')
 
+const router = require("./router/index")
 require( "dotenv" ).config( )
 
 const port = process.env.PORT || 5000
 const app = express()
+app.use(express.json())
+app.use(cookie_parser())
+app.use(cors())
+app.use("/api", router)
 
 const start = async() => {
     try {
-        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
+        //await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
         app.listen(port, () => {
             console.log("SERVER STARTED AT PORT " + port)
         })
+
     } catch(e) {
         console.log(e)
     }

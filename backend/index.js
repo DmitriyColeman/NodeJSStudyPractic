6286d6358e1dd3445f3b842d66d27348896a0a88
DmitriyColeman <47474886+DmitriyColeman@users.noreply.github.com>
2023-05-13 11:48:24 +0300

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require( "express" )
 const cors = require( "cors" )
 const cookie_parser = require( "cookie-parser" )
+const mongoose = require('mongoose')
+
 require( "dotenv" ).config( )
 
 const port = process.env.PORT || 5000
@@ -8,9 +10,9 @@ const app = express()
 
 const start = async() => {
     try {
+        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
         app.listen(port, () => {
             console.log("SERVER STARTED AT PORT " + port)
-
         })
     } catch(e) {
         console.log(e)
